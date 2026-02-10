f58c629783e34ccc087ca7679b21d68f86598ced
Divine-Wisdom <divinewisdomorima@gmail.com>
2026-02-10 16:30:11 +0100

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,8 +9,21 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 app.use('/api/auth', authRoutes);
-app.use('/api/auth', messageRoutes);
 app.use('/api/messages', messageRoutes);
+
+ // make ready for deployment
+ import path from "path"
+const __dirname = path.resolve();
+console.log("path", __dirname)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (_, res)=> {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+    });
+}
+
 const port = process.env.PORT || 4000; 
 
  app.listen(port, () => {
